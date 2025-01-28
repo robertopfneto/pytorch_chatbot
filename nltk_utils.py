63dde1f7b35bfe2db701e55d88f92b77feabe7c2
robertopfneto <neto2014roberto@gmail.com>
2025-01-28 01:47:42 -0300

    adicionando hyperparametros e rodando o treinamento

--- nltk_utils.py
+++ nltk_utils.py
@@ -10,11 +10,11 @@ stemmer = PorterStemmer()
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
-def setm(word):
+def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    tokenized_sentence = [setm(w) for w in tokenized_sentence]
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
     bag = np.zeros(len(all_words), dtype=np.float32)
 
     for idx, w in enumerate(all_words):
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import json
 import numpy as np
-from nltk_utils import bag_of_words, tokenize
-from nltk.stem.porter import PorterStemmer # for stemming
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize, stem
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
@@ -12,7 +12,7 @@ with open('intents.json', 'r') as f:
 all_words = []
 tags = []
 xy = []
-# loop through each sentence in our intents patterns
+
 for intent in intents['intents']:
     tag = intent['tag']
     tags.append(tag)
@@ -24,7 +24,6 @@ for intent in intents['intents']:
 
 ignore_words = ['?', '.', '!']
 
-stem = PorterStemmer()
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 
@@ -40,9 +39,8 @@ for (pattern_sentence, tag) in xy:
     label = tags.index(tag)
     y_train.append(label)
 
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-
+x_train = torch.tensor(x_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.long)
 
 class ChatDataset(Dataset):
     def __init__(self):
@@ -50,16 +48,45 @@ class ChatDataset(Dataset):
         self.x_data = x_train
         self.y_data = y_train
 
-    # dataset[idx]
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
 
     def __len__(self):
         return self.n_samples
-    
-# Hyperparameters
+
+batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(x_train[0])
+learning_rate = 0.001
+num_epochs = 10
+
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset,
-                          batch_size=8,
+                          batch_size=batch_size,
                           shuffle=True,
                           num_workers=2)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size=input_size,
+                  hidden_size=hidden_size,
+                  output_size=output_size).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch + 1) % 1 == 0:
+        print(f'fina loss, loss = {loss.item():.4f}')
